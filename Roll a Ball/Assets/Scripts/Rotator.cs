1933e302379263d93da8983396543f88e6f0fb1b
Rahul Dani <32138368+15dani1@users.noreply.github.com>
2019-03-06 01:08:45 -0500

    Added Roll a Ball Project

---
+++ Roll a Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);   
+    }
+}
